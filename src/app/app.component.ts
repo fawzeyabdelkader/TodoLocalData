2bbd340f389ff3624740c5819665319abe92c960
week-2 <fawzeyabdelkader32@gmail.com>
2024-06-23 04:06:05 +0300

    TodoLocalData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
+import { MatButtonModule } from '@angular/material/button';
+import { ButtonModule } from 'primeng/button';
+import { ToDoComponent } from './Components/to-do/to-do.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+   @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, ButtonModule,ToDoComponent,HeaderComponent,FooterComponent  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ToDoList';
+  title = 'TodoLocalData';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+ 
+  import { NotfoundComponent } from './Components/notfound/notfound.component';
+import { ToDoComponent } from './Components/to-do/to-do.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/todo', pathMatch: 'full' },
+
+
+
+  { path: 'todo', component: ToDoComponent,title:'TodoLocalData' },
+
+  { path: '**', component: NotfoundComponent,title:'Not Found' },
+
+ ];
